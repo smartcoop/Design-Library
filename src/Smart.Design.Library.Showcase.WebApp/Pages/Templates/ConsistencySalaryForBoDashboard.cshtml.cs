bdfb09d6dedf5c2e3e67a19fd89ef4df0b034a0c
Francois Legros <francois.legros@smart.coop>
2024-08-28 14:57:38 +0200

    ShowcaseWebApp, page "Consistency Salary For BO Dashboard": very first version of the page.

--- src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/ConsistencySalaryForBoDashboard.cshtml.cs
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/ConsistencySalaryForBoDashboard.cshtml.cs
@@ -1,11 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Smart.Design.Library.Showcase.Pages.Templates;
 
 public class ConsistencySalaryForBoDashboardModel : PageModel
 {
+    [BindProperty]
+    public DateTime FromDate { get; set; }
+
+    [BindProperty]
+    public DateTime ToDate { get; set; }
+
+    [BindProperty]
+    public float MinimumDelta { get; set; }
+
     public void OnGet()
     {
 
     }
+}
+
+public class ConsistencySalaryModel
+{
+    public float SamRawSalary { get; set; }
+
+    public float ForHrmRawSalary { get; set; }
 }
