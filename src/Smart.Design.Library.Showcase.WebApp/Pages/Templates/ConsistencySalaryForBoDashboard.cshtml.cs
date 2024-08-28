868264083882cd823af2d0927369455a97e49a83
Francois Legros <francois.legros@smart.coop>
2024-08-28 13:23:20 +0200

    ShowcaseWebApp: add page "Consistency Salary For BO Dashboard"

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/ConsistencySalaryForBoDashboard.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Templates;
+
+public class ConsistencySalaryForBoDashboardModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
--- src/Smart.Design.Library.Showcase.WebApp/Program.cs
+++ src/Smart.Design.Library.Showcase.WebApp/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.AspNetCore.Http.Json;
+using Newtonsoft.Json.Serialization;
 using Smart.Design.Library.Extensions;
 
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions {WebRootPath = "wwwroot"});
 builder.Services.AddKendo();
 
 // Add services to the container.
+builder.Services.AddMvc()
+    .AddNewtonsoftJson(options =>
+                       options.SerializerSettings.ContractResolver =
+                          new DefaultContractResolver());
 builder.Services.AddRazorPages();
 builder.Services.AddSmartDesign();
 builder.Services.AddKendo();
