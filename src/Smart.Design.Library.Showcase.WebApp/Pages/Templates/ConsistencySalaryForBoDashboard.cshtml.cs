76d7dbadb46e943ae6b1ffe27503d82c7356e4b0
Francois Legros <francois.legros@smart.coop>
2024-08-29 15:45:42 +0200

    ShowcaseWebApp, page "Consistency Salary For BO Dashboard": add some columns to the grid.

--- src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/ConsistencySalaryForBoDashboard.cshtml.cs
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/ConsistencySalaryForBoDashboard.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
 using Microsoft.AspNetCore.Mvc;
@@ -18,19 +19,35 @@ public class ConsistencySalaryForBoDashboardModel : PageModel
 
     private static readonly List<ConsistencySalaryAnomaly> consistencySalaryAnomalies = new List<ConsistencySalaryAnomaly>();
 
-    public void OnPostGenerateReport()
+    public IActionResult OnPostGenerateReport()
     {
-        var newAnomalyCount = Convert.ToInt32((ToDate - FromDate).Days * MinimumDelta);
-        for (int i = 0; i < newAnomalyCount; i++)
+        var random = new Random();
+        var newAnomalyCount = Convert.ToInt32((ToDate - FromDate).Days * 10);
+        for (int i = 1; i <= newAnomalyCount; i++)
         {
-            consistencySalaryAnomalies.Add(
-                new ConsistencySalaryAnomaly
-                {
-                    SamRawSalary = (i * 5.56f) % 10,
-                    ForHrmRawSalary = (i * 5.59f) % 10
-                }
-            );
+            ConsistencySalaryAnomaly anomaly = new ConsistencySalaryAnomaly
+            {
+                MemberReference = (i * 549 % 1000).ToString(CultureInfo.InvariantCulture),
+                WorkerReference = (i * 487 % 500).ToString(CultureInfo.InvariantCulture),
+                ContractReference = (i * 943 % 10000).ToString(CultureInfo.InvariantCulture),
+
+                ContractStartDate = FromDate.AddHours(random.Next(0, (int)(ToDate - FromDate).TotalHours)),
+                ContractDurationInDays = (i * 348 % 5) + 1,
+
+                SamRawSalary = i * 5.56f % 100,
+                ForHrmRawSalary = i * 6.02f % 100,
+
+                IsActive = Convert.ToBoolean(i * 307 % 10)
+            };
+            anomaly.RawSalaryDelta = anomaly.ForHrmRawSalary - anomaly.SamRawSalary;
+
+            if (anomaly.RawSalaryDelta > MinimumDelta)
+            {
+                consistencySalaryAnomalies.Add(anomaly);
+            }
         }
+
+        return Page();
     }
 
     public void OnPostFixIssues()
@@ -47,7 +64,24 @@ public class ConsistencySalaryForBoDashboardModel : PageModel
 
 public class ConsistencySalaryAnomaly
 {
+    public string MemberReference { get; set; } = string.Empty;
+
+    public string WorkerReference { get; set; } = string.Empty;
+
+    public string ContractReference { get; set; } = string.Empty;
+
+    public DateTime ContractStartDate { get; set; }
+
+    public int ContractDurationInDays { get; set; }
+
     public float SamRawSalary { get; set; }
 
     public float ForHrmRawSalary { get; set; }
+
+    /// <summary>
+    /// ForHrm Raw Salary - Sam Raw Salary
+    /// </summary>
+    public float RawSalaryDelta { get; set; }
+
+    public bool IsActive { get; set; }
 }
