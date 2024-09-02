3a91306c9a56d0fae952d17aefd706f6406fde9f
Francois Legros <francois.legros@smart.coop>
2024-09-02 11:39:12 +0200

    ShowcaseWebApp: add page "Consistency Day For BO Dashboard"

---
+++ src/Smart.Design.Library.Showcase.WebApp/Pages/Templates/ConsistencyDayForBoDashboard.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Design.Library.Showcase.Pages.Templates;
+
+public class ConsistencyDayForBoDashboardModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
